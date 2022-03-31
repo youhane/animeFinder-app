a800761aaaef1468c92516077586dbb6d1113551
youhane <jason.sebastian154@gmail.com>
2022-03-31 14:24:51 +0700

    app can detect and is connected to api, need styling tho

---
+++ static/predict.js
@@ -0,0 +1,74 @@
+let imageLoaded = false;
+$("#image-selector").change(function () {
+	imageLoaded = false;
+	let reader = new FileReader();
+	reader.onload = function () {
+		let dataURL = reader.result;
+		$(".selected-image").attr("src", dataURL);
+		$("#prediction-list").empty();
+		imageLoaded = true;
+	}
+	
+	let file = $("#image-selector").prop('files')[0];
+	reader.readAsDataURL(file);
+});
+
+let model;
+let modelLoaded = false;
+$( document ).ready(async function () {
+	modelLoaded = false;
+	$('.progress-bar').show();
+    console.log( "Loading model..." );
+    model = await tf.loadGraphModel('model/model.json');
+    console.log( "Model loaded." );
+	$('.progress-bar').hide();
+	modelLoaded = true;
+});
+
+$("#predict-button").click(async function () {
+	if (!modelLoaded) { alert("The model must be loaded first"); return; }
+	if (!imageLoaded) { alert("Please select an image first"); return; }
+	
+	let image = $('.selected-image').get(0);
+	
+	// Pre-process the image
+	console.log( "Loading image..." );
+	let tensor = tf.browser.fromPixels(image, 3)
+	.resizeNearestNeighbor([96,96])
+		.expandDims(0)
+		.div(tf.scalar(255.0))
+	let predictions = await model.predict(tensor).data();
+	console.log(predictions);
+	let top5 = Array.from(predictions)
+		.map(function (p, i) { // this is Array.map
+			return {
+				probability: p*10,
+				className: TARGET_CLASSES[i], // we are selecting the value from the obj
+				animeName: ANIME_LIST[i]
+			};
+		}).sort(function (a, b) {
+			return b.probability - a.probability;
+		}).slice(0, 5);
+
+	$("#prediction-list").empty();
+	top5.forEach(function (p) {
+		$("#prediction-list").append(`<li>${p.className} - ${p.animeName}: ${p.probability.toFixed(2)} %</li>`);
+	});
+
+	const inputAnimeName = top5[0].animeName
+	console.log(inputAnimeName)
+
+	// API JIKAN
+	const BASE_URL = 'https://api.jikan.moe/v4/'
+	let search = 'anime?q=' + inputAnimeName
+
+	$.ajax({
+		url: BASE_URL + search,
+		type: 'get',
+		dataType: 'json',
+		success: (result) => {
+			$("#prediction-list").html(`<h1>${result.data[0].title}</h1>`);
+			console.log(result.data)
+		}
+	})
+});
