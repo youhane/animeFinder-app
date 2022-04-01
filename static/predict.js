a53deb62d0927e4794b8dd856631f1236485ce71
youhane <jason.sebastian154@gmail.com>
2022-04-01 21:45:31 +0700

    logic pretty much done, needs to be styled

--- static/predict.js
+++ static/predict.js
@@ -1,16 +1,26 @@
+var imgSrc;
 let imageLoaded = false;
 $("#image-selector").change(function () {
 	imageLoaded = false;
 	let reader = new FileReader();
 	reader.onload = function () {
-		let dataURL = reader.result;
-		$(".selected-image").attr("src", dataURL);
+		imgSrc = reader.result;
+		$(".selected-image").attr("src", imgSrc);
 		$("#prediction-list").empty();
 		imageLoaded = true;
 	}
 	
 	let file = $("#image-selector").prop('files')[0];
 	reader.readAsDataURL(file);
+	$("#anime-title").text(" ");
+	$("#same-chara").empty();
+});
+
+$("#url-selector").change(function () {
+	let dataURL = $("#url-selector").val()
+	imageLoaded = false;
+	$(".selected-image").attr("src", dataURL);
+	imageLoaded = true;	
 	$("#anime-title").text(" ");
 });
 
@@ -31,12 +41,15 @@ $( document ).ready(async function () {
 const BASE_URL = 'https://api.jikan.moe/v4/'
 var similarChara
 
-
-
 $("#predict-button").click(async function () {
 	if (!modelLoaded) { alert("The model must be loaded first"); return; }
 	if (!imageLoaded) { alert("Please select an image first"); return; }
+
+	$("#same-chara").empty();
+	$("#morechara").removeClass("d-none");
+	$(".selected-image-res").attr("src", imgSrc);
 	
+	// betak imagenya
 	let image = $('.selected-image').get(0);
 	
 	// Pre-process the image
@@ -46,7 +59,7 @@ $("#predict-button").click(async function () {
 		.expandDims(0)
 		.div(tf.scalar(255.0))
 	let predictions = await model.predict(tensor).data();
-	console.log(predictions);
+	// console.log(predictions);
 	let top5 = Array.from(predictions)
 		.map(function (p, i) { // this is Array.map
 			return {
@@ -63,11 +76,12 @@ $("#predict-button").click(async function () {
 		$("#prediction-list").append(`<li>${p.className} - ${p.animeName}: ${p.probability.toFixed(2)} %</li>`);
 	});
 
+	$("#chara-name").text(`${top5[0].className} (${top5[0].probability.toFixed(2)} %)`);
+
 	const inputAnimeName = top5[0].animeName
 	similarChara = [top5[0].className, top5[1].className, top5[2].className, top5[3].className]
 
 	let search = 'anime?q=' + inputAnimeName
-	let animeId;
 
 	$.get(`${BASE_URL}${search}`, (res) => {
 		// Get Title
@@ -79,8 +93,8 @@ $("#predict-button").click(async function () {
 		$.get(`${BASE_URL}characters?q=${similarChara[0]}`, (res) => {
 			let charaId = res.data[0].mal_id
 			$.get(`${BASE_URL}characters/${charaId}/pictures`, (res) => {
-				for (let index = 1; index <= 3; index++) {
-					$("#same-chara").append(`<img src="${res.data[index].jpg.image_url}" />`)
+				for (let index = 1; index <= 4; index++) {
+					$("#same-chara").append(`<img src="${res.data[index].jpg.image_url}" width="70" />`)
 				}
 			})
 		})
@@ -88,13 +102,16 @@ $("#predict-button").click(async function () {
 });
 
 $('#morechara').click(() => {
+	$("#similar-chara").empty()
 	// Get other similar chara id
-	for (let i = 1; i <= 4; i++) {
-		$.get(`${BASE_URL}characters?q=${similarChara[i]}`, (res) => {
-			let charaId = res.data[0].mal_id
-			$.get(`${BASE_URL}characters/${charaId}/pictures`, (res) => {
-				$("#similar-chara").append(`<img src="${res.data[0].jpg.image_url}" /><p>${similarChara[i]}</p>`)
-			})
+	// CharaNumber
+	let charaNumber = Math.floor(Math.random()*similarChara.length)
+	charaNumber = charaNumber === 0 ? charaNumber+1 : charaNumber
+
+	$.get(`${BASE_URL}characters?q=${similarChara[charaNumber]}`, (res) => {
+		let charaId = res.data[0].mal_id
+		$.get(`${BASE_URL}characters/${charaId}/pictures`, (res) => {
+			$("#similar-chara").append(`<img src="${res.data[0].jpg.image_url}" /><p>${similarChara[charaNumber]}</p>`)
 		})
-	}
+	})
 })
